0f56ae3c6f78ee62a67deb7349aa847b28daf29a
Johan Planchon <dev@planchon.dev>
2023-02-06 14:03:06 +0100

    feat: added filestash

---
+++ modules/filestash/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "docker_network" "filestash" {
+  name = "filestash_internal"
+}
+
+resource "docker_image" "onlyoffice" {
+  name = "onlyoffice/documentserver"
+}
+resource "docker_image" "filestash" {
+  name = "machines/filestash"
+}
+
+resource "docker_container" "onlyoffice" {
+  name = "onlyoffice"
+  hostname = "onlyoffice"
+  image = docker_image.onlyoffice.image_id
+  restart = "unless-stopped"
+
+  networks_advanced {
+    name = docker_network.filestash.id
+  }
+
+  depends_on = [
+    docker_network.filestash,
+    docker_image.onlyoffice
+  ]
+}
+
+resource "docker_container" "filestash" {
+  name = "filestash"
+  hostname = "filestash"
+  image = docker_image.filestash.image_id
+  restart = "unless-stopped"
+
+  networks_advanced {
+    name = docker_network.filestash.id
+  }
+  networks_advanced {
+    name = var.network
+  }
+
+  env = [
+    "APPLICATION_URL=",
+    "ONLYOFFICE_URL=http://onlyoffice",
+    "CONFIG_SECRET=${var.config_secret}"
+  ]
+
+  volumes {
+    host_path = "/var/local/docker/filestash/config"
+    container_path = "/app/data/state"
+  }
+  volumes {
+    host_path = "/var/local/docker/filestash/data"
+    container_path = "/data"
+  }
+  volumes {
+    host_path = "/var/local/docker/shareftp/data/data"
+    container_path = "/other_data/share"
+  }
+
+  depends_on = [
+    docker_network.filestash,
+    docker_image.filestash
+  ]
+}
---
+++ modules/filestash/variables.tf
@@ -0,0 +1,8 @@
+variable "network" {
+  description = "Created container network"
+  type = string
+}
+variable "config_secret" {
+  description = "Secret to encrypt config"
+  type = string
+}
