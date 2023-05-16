caa3f7b17b6a4135bc49483cc110df258432c10c
Johan Planchon <dev@planchon.dev>
2023-05-16 18:13:15 +0200

    feat: updated node template with new params

--- modules/coder/templates/node/main.tf
+++ modules/coder/templates/node/main.tf
@@ -11,19 +11,34 @@ terraform {
   }
 }
 
-variable "git_repo" {
-  description = "Default git repository"
-  default = ""
-  sensitive = false
+provider "coder" {
+  feature_use_managed_variables = "true"
 }
-variable "node_version" {
+
+data "coder_parameter" "node_version" {
+  name        = "Node Version"
+  type        = "number"
   description = "Installed node version"
-  default = "16"
-  sensitive = false
+  mutable     = true
+  default     = "16"
+}
+
+data "coder_parameter" "git_repo" {
+  name        = "Git Repository"
+  type        = "string"
+  description = "Default git repository"
+  mutable     = true
+  default     = ""
+
+  validation {
+    regex = "^(git@([a-zA-Z0-9-_.]+):(([a-zA-Z0-9-_.~]+)\\/)+[a-zA-Z0-9-_.]+|(https:\\/\\/[a-zA-Z0-9-_.]+\\/([a-zA-Z0-9-_.]+\\/)+)[a-zA-Z0-9-_.]+|)$"
+    error = "Unfortunately, it isn't a supported git url"
+  }
 }
 
 locals {
   username = data.coder_workspace.me.owner
+  git_folder = data.coder_parameter.git_repo.value != "" ? regex("[a-zA-Z0-9-_]+/(?P<folder>[a-zA-Z0-9-_]+)(?P<git>.git)?$", data.coder_parameter.git_repo.value).folder : ""
 }
 
 data "coder_provisioner" "me" {}
@@ -41,10 +56,14 @@ resource "coder_agent" "main" {
     sudo chown coder:coder ~/.local
     sudo chown coder:coder ~/.local/share
 
-    if [ ! -z "${var.git_repo}" ]; then env GIT_SSH_COMMAND="$GIT_SSH_COMMAND -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone ${var.git_repo}; fi
+    if [ ! -z "${data.coder_parameter.git_repo.value}" ] && [ ! -d "${local.git_folder}" ]; then 
+      ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+      git clone ${data.coder_parameter.git_repo.value} ${local.git_folder}
+    fi
 
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh
+    code-server --install-extension usernamehw.errorlens
     code-server --auth none --port 13337
     
     EOF
@@ -59,13 +78,40 @@ resource "coder_agent" "main" {
     GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
     GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
   }
+  metadata {
+    display_name = "CPU Usage"
+    key = "cpu"
+    # calculates CPU usage by summing the "us", "sy" and "id" columns of
+    # vmstat.
+    script = <<EOT
+        top -bn1 | awk 'FNR==3 {printf "%2.0f%%", $2+$3+$4}'
+    EOT
+    interval = 1
+    timeout = 1
+  }
+  metadata {
+    display_name = "Memory Usage"
+    key = "mem"
+    script = <<EOT
+    cat /sys/fs/cgroup/memory.current | awk '{ printf("%.2fMB", $1/1024/1024) }'
+    EOT
+    interval = 1
+    timeout = 1
+  }
+  metadata {
+    display_name = "Process Count"
+    key = "proc"
+    script = "ps aux | wc -l"
+    interval = 1
+    timeout = 3
+  }
 }
 
 resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
   display_name = "code-server"
-  url          = "http://localhost:13337/?folder=/home/coder${var.git_repo != "" ? "/${regex("[a-zA-Z0-9-_]+/(?P<folder>[a-zA-Z0-9-_]+)(?P<git>.git)?$", var.git_repo).folder}" : ""}"
+  url          = "http://localhost:13337/?folder=/home/coder${local.git_folder}"
   icon         = "/icon/code.svg"
   subdomain    = false
   share        = "owner"
@@ -104,15 +150,24 @@ resource "docker_volume" "home_volume" {
     value = data.coder_workspace.me.name
   }
 }
+resource "coder_metadata" "home_volume" {
+  count = data.coder_workspace.me.start_count
+  resource_id = docker_volume.home_volume.id
+  hide = true
 
+  item {
+    key = "id"
+    value = docker_volume.home_volume.name
+  }
+}
 
 resource "docker_image" "main" {
-  name = "coder-node"
+  name = "coder-${data.coder_workspace.me.id}-node"
   build {
     context = "./build"
 
     build_arg = {
-      NODE_VERSION: var.node_version
+      NODE_VERSION: data.coder_parameter.node_version.value
     }
   }
   triggers = {
