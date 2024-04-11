2fa780c9ae5d08f78c9b33f72a5d20ee976bf592
Johan Planchon <dev@planchon.dev>
2024-04-11 17:24:51 +0200

    feat: updated concourse

--- modules/concourse/main.tf
+++ modules/concourse/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "docker_image" "concourse" {
-  name = "concourse/concourse:7.11.1"
+  name = "concourse/concourse:7.11.2"
 }
 
 resource "docker_network" "internal_concourse" {
