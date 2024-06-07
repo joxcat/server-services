1d216071f37d15e959cbc787d3e4d151a596988c
Johan Planchon <dev@johan.moe>
2024-06-07 14:09:57 +0200

    feat: updated pihole cors

--- modules/pihole/main.tf
+++ modules/pihole/main.tf
@@ -47,6 +47,7 @@ resource "docker_container" "pihole" {
   env = [
     "TZ=Europe/Paris",
     "DNSMASQ_LISTENING=all",
+    "CORS_HOSTS=hole.planchon.dev",
     "PIHOLE_DNS_=",
     #"PIHOLE_DNS_=9.9.9.10;149.112.112.10;1.1.1.1;1.0.0.1;8.8.8.8;8.8.4.4",
   ]
